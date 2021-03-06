6055f3a7a9a6eafa872cf06da8fb8020bcaefee3
Wangrz-NWPU <541447264@qq.com>
2021-03-06 14:02:38 +0800

    first commit

---
+++ src/test/java/zhe/study/demo/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package zhe.study.demo.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
